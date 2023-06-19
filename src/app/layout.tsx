6af54d4b51cfde26181a9690eb65bbd60c9837d9
TheRohit <rohitmeshram000@gmail.com>
2023-06-20 01:57:58 +0530

    use reactquery to create a community

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from '@/components/Navbar';
+import Providers from '@/components/Providers';
 import { Toaster } from '@/components/ui/Toaster';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css'
@@ -23,6 +24,7 @@ export default function RootLayout({
   return (
     <html lang='en' className={cn('bg-white text-slate-900 antialiased light',inter.className)}>
       <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        <Providers>
         {/* @ts-expect-error server component */}
         <Navbar/> 
 
@@ -31,6 +33,8 @@ export default function RootLayout({
         <div className='container max-w-7xl mx-auto h-full pt-12'>
         {children}
         </div>
+        </Providers> 
+    
         <Toaster />
         </body>
     </html>
