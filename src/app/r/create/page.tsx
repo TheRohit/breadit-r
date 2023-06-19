1e5f8906b9b8e12ff1915388413899c6db456ba7
TheRohit <rohitmeshram000@gmail.com>
2023-06-20 02:36:03 +0530

    handle create subreddit route and error handling

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -5,12 +5,16 @@ import { Input } from "@/components/ui/Input";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useMutation } from "@tanstack/react-query";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { CreateSubredditPayload } from "@/lib/validators/subreddit";
+import { toast } from "@/hooks/use-toast";
+import { useCustomToast } from "@/hooks/use-custom-toast";
 
 const Page = () => {
   const [input, setInput] = useState<string>("");
   const router = useRouter();
+  const {loginToast} = useCustomToast()
+
 
   const { mutate: createCommunity, isLoading } = useMutation({
     mutationFn: async () => {
@@ -20,6 +24,30 @@ const Page = () => {
       const { data } = await axios.post("/api/subreddit", payload);
       return data as string;
     },
+    onError:(err) =>{
+        if(err instanceof AxiosError){
+            if(err.response?.status === 409){
+                return toast({
+                    title: 'Subreddit already exists.',
+                    description:'Please choose a different subreddit name',
+                    variant:"destructive",
+                })
+            }
+
+            if(err.response?.status === 422){
+                return toast({
+                    title: 'Invalid subreddit name.',
+                    description:'Please choose a name between 3 and 12 characters.',
+                    variant:"destructive",
+                })
+            }
+
+            if(err.response?.status === 401){
+                return loginToast()
+            }
+        }
+    }
+
   });
 
   return (
