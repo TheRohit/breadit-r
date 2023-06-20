7ddbb4f0e2210c729e36f3f49fca3d78db28be00
TheRohit <rohitmeshram000@gmail.com>
2023-06-21 04:40:54 +0530

    joining the subreddit

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,3 +1,4 @@
+import SubscribeLeaveToggle from "@/components/SubscribeLeaveToggle";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import { format } from "date-fns";
@@ -78,8 +79,15 @@ const Layout = async ({
               </div>
               {subreddit.creatorId === session?.user.id ? (
                 <div className="flex justify-between gap-x-4 py-3 ">
-                    <p className="text-gray-500">You created this community</p>
+                  <p className="text-gray-500">You created this community</p>
                 </div>
+              ) : null}
+
+              {subreddit.creatorId !== session?.user.id ? (
+                <SubscribeLeaveToggle
+                  subredditId={subreddit.id}
+                  subredditName={subreddit.name}
+                />
               ) : null}
             </dl>
           </div>
