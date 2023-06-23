543c1f3c634bb01c2a437156cb7e3561d11fb7c5
TheRohit <rohitmeshram000@gmail.com>
2023-06-23 20:45:08 +0530

    implement infinite scrolling

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,11 +1,17 @@
 "use client"
 
+//for the errors which says you need to warp it inside providers
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { SessionProvider } from "next-auth/react"
 
 const Providers = ({children} : {children: React.ReactNode}) => {
     const queryClient = new QueryClient()
  return <QueryClientProvider client={queryClient}>
-    {children}
+   <SessionProvider>
+   {children}
+   </SessionProvider>
+    
  </QueryClientProvider>
 }
 
