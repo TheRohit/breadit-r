b264ed78582c7d93513cdaa726de7f1dc28f83c1
TheRohit <rohitmeshram000@gmail.com>
2023-06-26 22:15:15 +0530

    redis caching and vote api implemented

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -35,21 +35,21 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
   }, [initialVote]);
 
   //api using react query
-  const {} = useMutation({
+  const {mutate:vote} = useMutation({
     mutationFn: async (voteType:VoteType) => {
       const payload:PostVoteRequest= {
         postId,
         voteType,
       }
 
-      await axios.
+      await axios.patch('/api/subreddit/post/vote',payload)
     }
   })
 
 
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
-      <Button size="sm" variant="ghost" aria-label="upvote">
+      <Button onClick={() => vote('UP')} size="sm" variant="ghost" aria-label="upvote">
         <ArrowBigUp
           className={cn("h-5 w-5 text-zinc-700", {
             "text-emerald-500 fill-emerald-500": currentVote === "UP",
@@ -62,7 +62,7 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
         </p> 
 
 
-        <Button size="sm" variant="ghost" aria-label="downvote">
+        <Button onClick={() => vote('DOWN')} size="sm" variant="ghost" aria-label="downvote">
         <ArrowBigDown
           className={cn("h-5 w-5 text-zinc-700", {
             "text-emerald-500 fill-emerald-500": currentVote === "DOWN",
