8c10ac2d3ee4cae788952353b6d33bbf2f83285d
TheRohit <rohitmeshram000@gmail.com>
2023-06-27 00:08:49 +0530

    fix downvote button colour

--- src/components/post-vote/PostVoteClient.tsx
+++ src/components/post-vote/PostVoteClient.tsx
@@ -11,7 +11,8 @@ import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useMutation } from "@tanstack/react-query";
 import { PostVoteRequest } from "@/lib/validators/vote";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
 
 interface PostVoteClientProps {
   postId: string;
@@ -35,21 +36,60 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
   }, [initialVote]);
 
   //api using react query
-  const {mutate:vote} = useMutation({
-    mutationFn: async (voteType:VoteType) => {
-      const payload:PostVoteRequest= {
+  const { mutate: vote } = useMutation({
+    mutationFn: async (voteType: VoteType) => {
+      const payload: PostVoteRequest = {
         postId,
         voteType,
+      };
+
+      await axios.patch("/api/subreddit/post/vote", payload);
+    },
+    //if the action does not take place we revert it to previous one
+    onError: (err, VoteType) => {
+      if (VoteType === "UP") setVotesAmt((prev) => prev - 1);
+      else setVotesAmt((prev) => prev + 1);
+
+      //reset current vote 
+
+      setCurrentVote(prevVote)
+
+      if(err instanceof AxiosError){
+        if(err.response?.status === 401){
+          return loginToast()
+        }
       }
 
-      await axios.patch('/api/subreddit/post/vote',payload)
-    }
-  })
+      return toast({
+        title: 'Something went wrong',
+        description: 'Your vote was not registered, please try again.',
+        variant:'destructive'
+      })
+    },
+    //as soon as the action happens
 
+    onMutate:(type:VoteType) =>{
+      if(currentVote===type){
+        setCurrentVote(undefined)
+        if(type==='UP') setVotesAmt((prev) => prev-1)
+        if(type==='DOWN') setVotesAmt((prev) => prev+1) 
+      }
+      else{
+        setCurrentVote(type)
+        if(type==='UP') setVotesAmt((prev) => prev + (currentVote? 2: 1))
+        else if(type='DOWN') setVotesAmt((prev) => prev - (currentVote? 2: 1))
+      }
+    }
+  });
 
   return (
     <div className="flex sm:flex-col gap-4 sm:gap-0 pr-6 sm:w-20 pb-4 sm:pb-0">
-      <Button onClick={() => vote('UP')} size="sm" variant="ghost" aria-label="upvote">
+      <Button
+        onClick={() => vote("UP")}
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+      >
         <ArrowBigUp
           className={cn("h-5 w-5 text-zinc-700", {
             "text-emerald-500 fill-emerald-500": currentVote === "UP",
@@ -59,17 +99,20 @@ const PostVoteClient: FC<PostVoteClientProps> = ({
 
       <p className="text-center py-2 font-medium text-sm text-zinc-900">
         {votesAmt}
-        </p> 
+      </p>
 
-
-        <Button onClick={() => vote('DOWN')} size="sm" variant="ghost" aria-label="downvote">
+      <Button
+        onClick={() => vote("DOWN")}
+        size="sm"
+        variant="ghost"
+        aria-label="downvote"
+      >
         <ArrowBigDown
           className={cn("h-5 w-5 text-zinc-700", {
-            "text-emerald-500 fill-emerald-500": currentVote === "DOWN",
+            "text-red-500 fill-red-500": currentVote === "DOWN",
           })}
         />
       </Button>
-        
     </div>
   );
 };
